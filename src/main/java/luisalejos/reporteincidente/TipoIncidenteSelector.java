d0430ae17974bfdac90782297ce3f4d343d1ff3c
alex <darkeitor6@outlook.com>
2025-06-11 17:46:04 -0500

    haciendo panel de seleccion de tipo de accidente

---
+++ src/main/java/luisalejos/reporteincidente/TipoIncidenteSelector.java
@@ -0,0 +1,139 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package luisalejos.reporteincidente;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Paint;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.border.Border;
+
+/**
+ *
+ * @author alexis
+ */
+public class TipoIncidenteSelector extends JPanel {
+    
+    private String tipo;
+
+    public TipoIncidenteSelector() {
+    
+        setPreferredSize(new Dimension(480, 360));
+        
+        ButtonGroup tipoIncidente = new ButtonGroup();
+        JRadioButton incidenteTecnico = new JRadioButton("1");
+        JRadioButton incidenteSeguridad = new JRadioButton("");
+        JRadioButton incidenteInstalacion = new JRadioButton("3");
+        
+        incidenteSeguridad.setActionCommand("seguridad");
+        
+        Border bordeSeleccionado = BorderFactory.createDashedBorder(Color.BLUE, 3,2,2,true);
+        Border bordeVacio = BorderFactory.createEmptyBorder(2, 2, 2, 2); // 
+       
+        
+        ImageIcon iconoSeguridad = new ImageIcon("iconos/seguridad.png", "seguridad");
+        
+        incidenteSeguridad.setBorder(bordeVacio);
+        incidenteSeguridad.setBorderPainted(true);
+        
+        
+        System.out.println(TipoIncidenteSelector.class.getResource("src/iconos/seguridad.png"));
+        
+        incidenteSeguridad.setIcon(iconoSeguridad);
+       
+        
+        
+        
+        
+        JButton test = new JButton("test");
+        
+        ActionListener listener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JRadioButton boton = (JRadioButton) e.getSource();
+              
+                tipo = boton.getActionCommand();
+                System.out.println("Opción seleccionada: " + tipo);
+            }
+        };
+
+        ItemListener listener2 = e -> {
+                JRadioButton boton = (JRadioButton) e.getSource();
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    // 3. Aplicar el borde seleccionado
+                    
+                    boton.setBorder(bordeSeleccionado);
+                } else if (e.getStateChange() == ItemEvent.DESELECTED) {
+                    // 3. Quitar el borde (volver al vacío)
+                    boton.setBorder(bordeVacio);
+                }
+            };
+        
+        
+        
+        
+        
+        tipoIncidente.add(incidenteTecnico);
+        tipoIncidente.add(incidenteSeguridad);
+        tipoIncidente.add(incidenteInstalacion);
+        
+        incidenteInstalacion.addActionListener(listener);
+        incidenteTecnico.addActionListener(listener);
+        incidenteSeguridad.addActionListener(listener);
+        
+        incidenteSeguridad.addItemListener(listener2);
+        
+        add(incidenteInstalacion);
+        add(incidenteTecnico);
+        add(incidenteSeguridad);
+        
+        
+       
+    
+        add(test);
+        
+        
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            
+            JFrame ventana = new JFrame();
+            
+            
+            
+            
+            public void run() {
+                ventana.setDefaultCloseOperation(3);
+                ventana.setLocationRelativeTo(null);
+                
+                
+                
+                ventana.add(new TipoIncidenteSelector());
+                
+                ventana.pack();
+                ventana.setVisible(true);
+            }
+        });
+        
+    }
+    
+    
+    
+}
